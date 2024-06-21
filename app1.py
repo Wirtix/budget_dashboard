2293a3ed781b6bd5cb75ccbb19007dd0654ddee9
Wirtix <oskar0skutnik@gmail.com>
2024-06-21 11:25:48 +0200

    color s

--- app1.py
+++ app1.py
@@ -17,7 +17,7 @@ datemap = {1: 'styczeń',
            9:'wrzesień'
 }
 month_now = [v for k, v in datemap.items() if m == k]
-custom_colors = ['#FFD0EC', '#FFCC99', '#FFFF99', '#99FF99']
+custom_colors = ['C7DCA7']
 
 st.set_page_config(page_title='Sales Dashboard',
                    page_icon=':bar_char:',
