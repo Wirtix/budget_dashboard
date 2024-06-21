da665e934267b5534c2cf254716301842e03306b
Wirtix <oskar0skutnik@gmail.com>
2024-06-21 21:23:08 +0200

    wq q sasa

--- app1.py
+++ app1.py
@@ -25,7 +25,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 gf = conn.read(
     worksheet="savings",
